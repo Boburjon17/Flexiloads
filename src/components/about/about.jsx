b36343c31b24baa07746903cf2690d5d44baa471
Boburjon17 <boburjonsodiqov19@gmail.com>
2025-06-25 10:07:47 +0500

    unfinished

---
+++ src/components/about/about.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import "./about.scss"
+
+const About = () => {
+  return (
+    <div className='about-container'>
+        <div className="about">
+            <p>About</p>
+            <div className="about-title">
+                <h1>Solution for your business need</h1>
+                <p>We make logistic shipping much easier and straighforward. Combining good service and technology make everything efficient</p>
+            </div>
+            <div className="boxes">
+                <div className="box">
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default About
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from '../components/navbar/navbar'
 import Header from '../components/header/header'
+import About from '../components/about/about'
 
 const Home = () => {
   return (
     <div>
       <Navbar/>
       <Header/>
+      <About/>
     </div>
   )
 }
